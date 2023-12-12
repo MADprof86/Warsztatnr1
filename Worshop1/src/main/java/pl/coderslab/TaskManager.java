2158c0a0923a1fab9a0b4f829de1ec15fe62ddfb
madprof <krzys.zarzycki@gmail.co>
2023-12-12 20:47:56 +0100

    second

--- Worshop1/src/main/java/pl/coderslab/TaskManager.java
+++ Worshop1/src/main/java/pl/coderslab/TaskManager.java
@@ -1,9 +1,11 @@
 package pl.coderslab;
 import org.apache.commons.lang3.StringUtils;
 
+
 public class TaskManager{
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println(ConsoleColors.RED + "This is red");
   }
+
 }
