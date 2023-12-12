cc3388d8864df5e5e6ab6a8ac97042c97dc61eaa
madprof <krzys.zarzycki@gmail.co>
2023-12-12 20:24:36 +0100

    First commit

---
+++ Worshop1/src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,9 @@
+package pl.coderslab;
+import org.apache.commons.lang3.StringUtils;
+
+public class TaskManager{
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
