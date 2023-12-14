478c329b3f9f7a663ab40ebef994af90228a02cd
madprof <krzys.zarzycki@gmail.co>
2023-12-14 10:31:32 +0100

    remove

--- Worshop1/src/main/java/pl/coderslab/TaskManager.java
+++ Worshop1/src/main/java/pl/coderslab/TaskManager.java
@@ -7,32 +7,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class TaskManager{
+public class TaskManager {
 
   public static void main(String[] args) {
-   runProgram();
+    /*String[][] inputArray = {
+        {"Simple task - very important", "2020-03-09", "true"},
+        {"Second task not so important", "2020-05-10", "false"},
+        {"Throw away trash", "2020-03-09", "false"},
+        {"Bardzo skomplikowany task który posida więcej niż kilka wyrazów", "2023-12-14", "true"}};
+    inputArray = removeElementFromArray(inputArray,3);
+    for (String[] row: inputArray) {
+      System.out.println(Arrays.toString(row));
+
+    }*/
+    runProgram();
 
   }
-  public static void runProgram(){
+
+  public static void runProgram() {
     String[][] taskDataBase;
-    try{
+    try {
       taskDataBase = getTaskManagerDataBase("src/main/java/pl/coderslab/tasks.csv");
       showUIOptions(taskDataBase);
-    }
-    catch (IOException ex)
-    {
+    } catch (IOException ex) {
       System.out.println("Problem during dataBase reading" + ex.getMessage());
     }
 
   }
-  public static void showUIOptions(String[][] taskDataBase){
+
+  private static void showUIOptions(String[][] dataBase) {
+    String[][] taskManagerDataBase = dataBase;
     String[] uiMenuOptions = {"add", "remove", "list", "exit"};
-    String prompt = "Please select an option: ";
+    String prompt = "\nPlease select an option: ";
     String scannerInput;
     Scanner scanner = new Scanner(System.in);
     boolean terminateProgram = false;
 
-    while (!terminateProgram){
+    while (!terminateProgram) {
+
       System.out.println(ConsoleColors.BLUE + prompt + ConsoleColors.RESET);
       for (String option : uiMenuOptions
       ) {
@@ -40,69 +52,140 @@ public class TaskManager{
       }
       scannerInput = scanner.nextLine().toLowerCase().trim();
 
-      switch (scannerInput){
-          case "add" :
-            //AddTask()
-            break;
+      switch (scannerInput) {
+        case "add":
+          //AddTask()
+          break;
 
-          case "remove" :
-            //removeTask();
-            break;
+        case "remove":
+          String[][] newDatabase = removeTask(taskManagerDataBase,scanner);
+          if(newDatabase!= null) taskManagerDataBase=newDatabase;
 
-          case "list" :
-            //showTaskList();
-            break;
+          break;
 
-          case "exit" :
-            //exitProgram();
-            terminateProgram = true;
-            break;
+        case "list":
+          showTaskList(taskManagerDataBase);
+          break;
 
-          default: System.out.println(
+        case "exit":
+          //exitProgram();
+          terminateProgram = true;
+          break;
+
+        default:
+          System.out.println(
               ConsoleColors.RED +
                   "No such option. Please try again." +
-              ConsoleColors.RESET);
+                  ConsoleColors.RESET);
       }
-      if(terminateProgram) {
+      if (terminateProgram) {
         System.out.println("Exiting program!");
 
       }
     }
   }
-  public static String[][] resizeArray(String[][] arrayToResize){
-    return Arrays.copyOf(arrayToResize,arrayToResize.length*2);
+
+  private static String[][] resizeArray(String[][] arrayToResize) {
+    return Arrays.copyOf(arrayToResize, arrayToResize.length+1);
   }
 
-  public static String[][] getTaskManagerDataBase(String file) throws IOException {
-    String[][] taskManagerDataBase = new String[10][2];
+  private static String[][] getTaskManagerDataBase(String file) throws IOException {
+    String[][] taskManagerDataBase = new String[0][0];
     int arraySizeCounter = 0;
     Path filePath = Paths.get(file);
-    if(Files.exists(filePath)){
-      try (Scanner fileScanner = new Scanner(filePath)){
-        while(fileScanner.hasNextLine())
-        {
+    if (Files.exists(filePath)) {
+      try (Scanner fileScanner = new Scanner(filePath)) {
+        while (fileScanner.hasNextLine()) {
           String inputLine = fileScanner.nextLine().trim();
-          if(arraySizeCounter == taskManagerDataBase.length) {
-           taskManagerDataBase = resizeArray(taskManagerDataBase);
+          if (arraySizeCounter == taskManagerDataBase.length) {
+            taskManagerDataBase = resizeArray(taskManagerDataBase);
           }
           String[] task = inputLine.split(",");
-          if(task.length != 3) {
+          if (task.length != 3) {
             System.out.println(
                 ConsoleColors.RED +
                     "Task corrupted while reading database" +
-                ConsoleColors.RESET);
-          }
-          else {
+                    ConsoleColors.RESET);
+          } else {
             taskManagerDataBase[arraySizeCounter] = task;
             arraySizeCounter++;
           }
         }
       }
-    }
-    else throw new IOException ("The file does not exist");
+    } else
+      throw new IOException("The file does not exist");
     return taskManagerDataBase;
   }
 
+  private static void showTaskList(String[][] dataBase) {
+      int firstColumnMaxWidth = 30;
+   // System.out.printf("%-" + firstColumnMaxWidth + "s", line.substring(startIndex, endIndex));
+    for (int i = 0; i < dataBase.length; i++) {
+      String[] task = dataBase[i];
+      System.out.print(i + " : ");
+      for (String element : task) {
+        if (element != null) {
+          printWrappedLine(element, firstColumnMaxWidth,i);
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  private static void printWrappedLine(String line, int columnWidth, int lineNumber ) {
+    int timesToPrint = (int) Math.ceil((double) line.length()/columnWidth);
+    for(int i = 0; i< timesToPrint; i++){
+      int startIndex = i*columnWidth;
+      int endIndex = Math.min((i+1)*columnWidth,line.length());
+      String wrapperLogic =line.substring(startIndex, endIndex);
+
+      if(i == 0){
+        System.out.printf("%-" + columnWidth + "s", wrapperLogic);
+      }
+      else if(i == timesToPrint-1 ){
+              System.out.printf("%-" + (columnWidth+4) + "s",lineNumber + " : " + wrapperLogic);
+      }
+      else System.out.printf("\n"+"%-" + columnWidth + "s%n",lineNumber + " : " + wrapperLogic);
+      }
+    }
+    private static Integer tryParse(String value){
+      try { return Integer.parseInt(value);
+      }
+      catch (NumberFormatException e) {return null;}
+    }
+    private static String[][] removeElementFromArray(String[][] inputArray, int indexToRemove){
+      String[][] newArray = new String[inputArray.length-1][];
+      System.arraycopy(inputArray,0, newArray, 0,indexToRemove);
+      System.arraycopy(inputArray,indexToRemove+1,newArray,indexToRemove, inputArray.length - indexToRemove-1);
+      return newArray;
+    }
+
+  private static String[][] removeTask(String[][] dataBase, Scanner scanner){
+    String prompt = "Provide the task number to delete or type quit";
+      while(true)
+      {
+        System.out.println(ConsoleColors.BLUE + prompt + ConsoleColors.RESET);
+        if(scanner.hasNextInt()){
+          int taskNumber = scanner.nextInt();
+          if(taskNumber< dataBase.length && taskNumber > 0){
+            dataBase = removeElementFromArray(dataBase,taskNumber-1);
+            System.out.printf("Task number %d has been deleted",taskNumber);
+            break;
+          }
+          else System.out.println("No such number, try again or type quit");
+        }
+        else {
+          String userInput = scanner.nextLine().toLowerCase().trim();
+          if (userInput.equals("quit")) {
+            return null;
+          } else {
+            System.out.println("Wrong input. Try again.");
+          }
+        }
+
+      }
+      return dataBase;
+  }
 }
 
 
